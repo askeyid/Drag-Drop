e99d75f050f62208bd7ba45383d474ddd6d23c5c
Aleksandr Malygon <a_m_style@outlook.com>
2024-03-17 19:43:22 +0200

    add ProjectList class

--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,34 @@ function Sealed(target: Function) {
     Object.seal(target.prototype);
 }
 
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
 // ProjectInput Class
 // @Sealed
 class ProjectInput {
@@ -133,3 +161,5 @@ class ProjectInput {
 }
 
 const input = new ProjectInput();
+const activeList = new ProjectList("active");
+const finishedList = new ProjectList("finished");
